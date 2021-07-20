c9416218a0f4d29bb79388fdf0cbfc5793b69703
heesoolim <hlim29@my.centennialcollege.ca>
2021-07-20 10:59:25 -0400

    made home page responsive and change navbar colour as the scroll goes down

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -7,14 +7,14 @@ import { Utils } from 'src/app/services/Utils';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  toggleCollapse: Function;
+  // Variables that determines the navigation bar's colour
+  isMenuVisible: boolean;
+  isScrollDowned: boolean;
 
   constructor(private utils: Utils) {
   }
 
   ngOnInit() {
-    this.toggleCollapse = this.utils.toggleCollapse;
-
     // Get navbar element to change its colour when scroll down
     let navbar = <HTMLElement>document.querySelector('nav.navbar');
 
@@ -22,15 +22,57 @@ export class HeaderComponent implements OnInit {
     window.onscroll = () => {
       // If current scroll bar's position is greater than or equal to the navbar's height
       if (document.body.scrollTop >= navbar.clientHeight || document.documentElement.scrollTop >= navbar.clientHeight) {
-        // Make the background dark
-        navbar.classList.add("bg-dark");
-        navbar.classList.remove("bg-transparent");
+        // Set the value to make the background dark
+        this.isScrollDowned = true;
       }
       else {
-        // Otherwise, make the background transparent
-        navbar.classList.add("bg-transparent");
-        navbar.classList.remove("bg-dark");
+        // Set the value to make the background dark
+        this.isScrollDowned = false;
       }
+      // Change the navigation bar colour based on the scroll position and the visibility of the nav menu
+      this.changeNavbarColorDark(this.isScrollDowned || this.isMenuVisible);
     };
+  }
+
+  /**
+   * Method executed when the menu button is clicked in a small screen.
+   * - Change the background of the navbar.
+   * - Open or close the navbar menu.
+   *
+   * @memberof HeaderComponent
+   */
+  onClickNavbarMenu() {
+    // nav menu to change visibility
+    let navMenu = document.querySelector('#navbarSupportedContent');
+
+    // Change the visibility of the menu
+    this.utils.toggleCollapse(navMenu);
+
+    // Set the value if the nav menu is visible
+    this.isMenuVisible = document.querySelector('#navbarSupportedContent').classList.contains('show');
+
+    // Change the navigation bar colour based on the scroll position and the visibility of the nav menu
+    this.changeNavbarColorDark(this.isScrollDowned || this.isMenuVisible);
+  }
+
+  /**
+   * Change the colour of the navbar to dark or transparent
+   *
+   * @param {boolean} isDark If this value is true, navbar's colour is changed to dark.
+   * @memberof HeaderComponent
+   */
+  changeNavbarColorDark(isDark: boolean) {
+    // Get navbar element to change its colour when scroll down
+    let navbar = <HTMLElement>document.querySelector('nav.navbar');
+
+    // If is dark, change its colour to dark
+    if (isDark) {
+      navbar.classList.add("bg-dark");
+      navbar.classList.remove("bg-transparent");
+    }
+    else {
+      navbar.classList.remove("bg-dark");
+      navbar.classList.add("bg-transparent");
+    }
   }
 }
