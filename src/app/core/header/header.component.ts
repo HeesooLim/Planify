9187e669681d7932dbb1512e2f784fcafff82be8
heesoolim <hlim29@my.centennialcollege.ca>
2021-07-20 09:53:20 -0400

    changed the navbar color when scroll down and up and adjusted scrollbar in the detailbox

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -14,6 +14,23 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.toggleCollapse = this.utils.toggleCollapse;
-  }
 
+    // Get navbar element to change its colour when scroll down
+    let navbar = <HTMLElement>document.querySelector('nav.navbar');
+
+    // When the user scrolls down or up
+    window.onscroll = () => {
+      // If current scroll bar's position is greater than or equal to the navbar's height
+      if (document.body.scrollTop >= navbar.clientHeight || document.documentElement.scrollTop >= navbar.clientHeight) {
+        // Make the background dark
+        navbar.classList.add("bg-dark");
+        navbar.classList.remove("bg-transparent");
+      }
+      else {
+        // Otherwise, make the background transparent
+        navbar.classList.add("bg-transparent");
+        navbar.classList.remove("bg-dark");
+      }
+    };
+  }
 }
