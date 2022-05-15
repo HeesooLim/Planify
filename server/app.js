9da7e639bdb83ec02b354772ea23cb319f74ac4c
heesoolim <heesoo474@gmail.com>
2022-05-15 17:12:44 -0400

    updated server

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ const app = {
   server: express(),
   router: express.Router(),
   // clientDir: path.join(__dirname, "./../client/dist/client"),
-  clientDir: path.join(__dirname, "dist/client"),
+  clientDir: path.join(__dirname, "./../client/dist/client"),
   logger: logger,
 };
 
